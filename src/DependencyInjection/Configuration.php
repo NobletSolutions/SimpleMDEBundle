683641dc1a168dd116e545f19ff8025ed83e01a6
Dan Abrey <danabrey@gmail.com>
2018-06-07 10:03:31 +0100

    Add 'ns_' namespace to root node

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('simplemde');
+        $treeBuilder->root('ns_simplemde');
         return $treeBuilder;
     }
 }
